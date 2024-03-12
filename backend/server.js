7a2209ce7898ca4e093d81734dec2672830f3c0f
OmkarTuptewar <omkarsanjay2004@gmail.com>
2024-03-12 18:25:18 +0530

    setup done for GenZConnect

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express= require("express");
+const dotenv= require("dotenv");
+const {chats}= require("./data/data.js")
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.send("running on API successfully");
+
+});
+dotenv.config();
+
+
+app.get('/api/chat',(req,res)=>{
+    res.send(chats);
+})
+
+const PORT=process.env.PORT || 5000
+
+
+app.listen(5000,console.log(`port running on ${PORT} `));
