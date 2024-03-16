4eea5162a910531ebfc81eab43d5123b163d096f
OmkarTuptewar <omkarsanjay2004@gmail.com>
2024-03-16 16:02:16 +0530

    loginpages&frontend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const express= require("express");
 const dotenv= require("dotenv");
-const {chats}= require("./data/data.js")
+const {chats}= require("./data/data.js");
+const connectDB = require("./config/db.js");
 const app=express()
 
 app.get("/",(req,res)=>{
     res.send("running on API successfully");
 
 });
+    
 dotenv.config();
+connectDB();
 
 
 app.get('/api/chat',(req,res)=>{
