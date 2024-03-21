55a752209899c39a3c9b41a8b8a0dcd6938dcec5
OmkarTuptewar <omkarsanjay2004@gmail.com>
2024-03-22 02:12:00 +0530

    done building chat api

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const UserModel = mongoose.Schema(
+const userSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     pic: {
       type: String,
-      required: true,
-      default:
-        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+      default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
   },
   {
@@ -17,5 +16,18 @@ const UserModel = mongoose.Schema(
   }
 );
 
-const User = mongoose.Model("User", UserModel);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
 module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,29 @@ const express= require("express");
 const dotenv= require("dotenv");
 const {chats}= require("./data/data.js");
 const connectDB = require("./config/db.js");
-const app=express()
+const userRoutes=require("./routes/userRoutes.js");
+const chatRoutes=require("./routes/chatRoutes.js");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware.js");
 
+const app=express()
+app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("running on API successfully");
 
 });
+
     
 dotenv.config();
 connectDB();
 
 
-app.get('/api/chat',(req,res)=>{
-    res.send(chats);
-})
+app.use('/api/user',userRoutes);
+app.use('/api/chat',chatRoutes);
+
+
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT=process.env.PORT || 5000
 
